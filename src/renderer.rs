29da61d918c701b965f654fcbb02ccd5dd8059e6
Evan Anderson <evan.w.and@gmail.com>
2022-04-13 22:42:41 -0400

    prototype rendering of title and background image

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,3 +1,6 @@
+use std::path::Path;
+
+use sdl2::image::LoadTexture;
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
@@ -17,7 +20,30 @@ pub fn render(
     canvas.set_draw_color(background);
     canvas.clear();
 
+    let texture_creator = canvas.texture_creator();
+
     let (width, height) = canvas.output_size()?;
+
+    let background_texture = texture_creator.load_texture("assets/background.png")?;
+    canvas.copy(&background_texture, None, Rect::new(0, 0, width, height))?;
+
+    // title rendering
+    let font_path = Path::new("assets/mudclub.otf");
+    let ttf_context = sdl2::ttf::init().unwrap();
+    let font = ttf_context.load_font(font_path, 50).unwrap();
+    let surface = font
+        .render("logistics")
+        .blended(Color::RGBA(255, 87, 0, 255))
+        .unwrap();
+    let font_texture = texture_creator
+        .create_texture_from_surface(&surface)
+        .unwrap();
+    canvas.copy(
+        &font_texture,
+        None,
+        Rect::from_center(Point::new(200, 200), 200, 100),
+    )?;
+
 
     for (pos, sprite) in (&data.0, &data.1).join() {
         let current_frame = sprite.region;
